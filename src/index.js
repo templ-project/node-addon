75b413cc9ceac2e063d50872ad6a972fd5691d58
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-11-05 23:44:21 +0200

    refactor: github actions - debug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 module.exports = require('bindings')('main');
+
+/*******************************************************************************
+ * Document here your exported functions.
+ * Otherwise, `npm run docs` will not function.
+ ******************************************************************************/
+
+/**
+ * Hello *! function
+ * @param {string} name
+ * @return {string}
+ */
+function hello(name) {}
